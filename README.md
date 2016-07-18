02e67e3b6eabe4c20a71bf6199e36768c0cb217d
xiz <shizuka.kakimoto@linkage.jp.net>
2016-07-18 17:20:55 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# slack-notifier
+Notifiers for slack
+
+# Notifier list
+## weather-notifier
+### Do what
+Notify weather of any location in japan
+### Usage
+`ruby weather-notifier.rb 130010`
+### Hint
+Find the location id from the [Weather Hacks](http://weather.livedoor.com/forecast/rss/primary_area.xml) site.
---
+++ weather-notifier.rb
@@ -0,0 +1,24 @@
+require 'json'
+require 'open-uri'
+require 'slack-incoming-webhooks'
+
+if ARGV[0] =~ /^[0-9]{6}$/ # check if a number of 6 digits
+  uri = "http://weather.livedoor.com/forecast/webservice/json/v1?city=#{ARGV[0]}"
+end
+
+res     = JSON.load(open(uri).read)
+title   = res['title']
+text    = res['description']['text']
+link    = res['link']
+weather = res['forecasts'].first
+
+attachments = [{
+  title: title,
+  title_link: link,
+  text: text,
+  image_url: weather['image']['url'],
+  color: "#7CD197"
+}]
+
+slack = Slack::Incoming::Webhooks.new ENV['WEBHOOK_URL']
+slack.post "#{weather['date']}の#{title}は「#{weather['telop']}」です。", attachments: attachments
